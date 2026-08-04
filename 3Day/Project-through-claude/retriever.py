69cfa0bd237c2f8bf840b5728128d688d65d6b5a
poojaalld <poojaalld@yahoo.com>
2026-08-04 22:54:33 +0530

    Delete 3Day directory

--- 3Day/Project-through-claude/retriever.py
+++
@@ -1,7 +0,0 @@
-"""Similarity search retriever over the vector store.
-
-Implemented in Part 6.
-"""
-
-if __name__ == "__main__":
-    raise NotImplementedError("retriever.py will be implemented in Part 6")
